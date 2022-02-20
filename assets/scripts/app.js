1a084d63463ec0edafbbadaa8fad330b79a43ce6
Mateusz Kośmiński <kosminskimateusz@gmail.com>
2022-02-20 14:36:53 +0100

    All buttons have event listener

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,13 +1,47 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function add() {
-  const calcDescription = `${currentResult} + ${userInput.value}`;
-  // currentResult = currentResult + parseInt(userInput.value);
-  currentResult = currentResult + +userInput.value;
+function getUserInput() {
+  return +userInput.value;
+}
+
+function createAdnWriteOutput(operator, resultBeforeCalc, calcNumber) {
+  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
   outputResult(currentResult, calcDescription);
-  console.log("OK");
+}
+function add() {
+  const enteredNumber = getUserInput();
+  const initialResult = currentResult;
+  // currentResult = currentResult + parseInt(userInput.value);
+  currentResult += enteredNumber;
+  createAdnWriteOutput('+', initialResult, enteredNumber);
+}
+
+function subtract() {
+  const enteredNumber = getUserInput();
+  const initialResult = currentResult;
+  currentResult -= enteredNumber;
+  createAdnWriteOutput('-', initialResult, enteredNumber);
+}
+
+function multiply() {
+  const enteredNumber = getUserInput();
+  const initialResult = currentResult;
+  currentResult *= enteredNumber;
+  createAdnWriteOutput('*', initialResult, enteredNumber);
+}
+
+function divide() {
+  const enteredNumber = getUserInput();
+  const initialResult = currentResult;
+  if (enteredNumber !== 0) {
+    currentResult /= enteredNumber;
+    createAdnWriteOutput('/', initialResult, enteredNumber);
+  } else { outputResult(currentResult, 'Divide by 0 not allowed')}
 }
 
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
 
